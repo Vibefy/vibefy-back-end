1875265cc8c70714934d1a7e89fd6e71aa411824
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-10-30 11:09:50 -0300

    feat: Initialized project

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,13 @@
+import * as express from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        id: string;
+        isAdm: boolean;
+      };
+    }
+  }
+}
+export {};
