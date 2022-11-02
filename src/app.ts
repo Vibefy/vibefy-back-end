2bb6a32a6d7cb12c6a46bf340ece071b86d01342
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2022-11-02 18:46:08 -0300

    feat:swagger-basics-implementation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { sessionRoute } from "./routes/session.route";
 import { userRoute } from "./routes/user.route";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
-const app = express()
+const app = express();
 
-app.use("/user",userRoute)
-app.use("/login",sessionRoute)
+app.use("/user", userRoute);
+app.use("/login", sessionRoute);
 
-export default app
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.get("/terms", (req: Request, res: Response) => {
+  return res.json({
+    message: "Termos de Serviço",
+  });
+});
+
+export default app;
