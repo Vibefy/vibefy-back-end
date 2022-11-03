1dde159579bd4a4f0f63dee4d3fe7ceefd94336b
Gabriel <gabriellf.dev@gmail.com>
2022-11-03 12:49:29 -0300

    add: replace-caracter-especial/aws

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -8,8 +8,16 @@ export default {
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
-    
-      return callback(null, `${file.originalname}`);
+      const fileNameCorrect = file.originalname.replace(/[^a-zA-Z0-9 ]/g, '')
+      if(fileNameCorrect.includes("mp3")){
+        return callback(null, fileNameCorrect.replace("mp3","") + ".mp3");
+      }else if(fileNameCorrect.includes("png")){
+        return callback(null, fileNameCorrect.replace("png","") + ".png");
+      }else if(fileNameCorrect.includes("jpg")){
+        return callback(null, fileNameCorrect.replace("jpg","") + ".jpg");
+      }else{
+        return callback(null, fileNameCorrect.replace("jpge","") + ".jpge");
+      }
     },
   }),
 };
