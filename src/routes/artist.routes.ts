cbfd2368e65ad12c65621dbe94476a1f7113c728
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2022-11-05 15:16:30 -0300

    feat: criando a rota de get de artist

--- src/routes/artist.routes.ts
+++ src/routes/artist.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { createArtistController } from "../controllers/artist/createArtist.controller";
+import { getArtistController } from "../controllers/artist/getArtist.controller";
+import { verifyAuthTokenMiddleware } from "../middleware/verifyAuthTokenMiddleware";
 
 const artistRouter = Router();
 artistRouter.post("", createArtistController);
-
+artistRouter.get("/profile",verifyAuthTokenMiddleware, getArtistController)
 export { artistRouter };
