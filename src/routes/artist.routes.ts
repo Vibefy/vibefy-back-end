5a3612b6b30dba4da43b37cc56e02e95f6c8fb41
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2022-11-05 16:33:04 -0300

    fix: adicionando rota de delete

--- src/routes/artist.routes.ts
+++ src/routes/artist.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { deleteArtistController } from "../controllers/artist/artistDelet.controller";
 import { artistUpdateController } from "../controllers/artist/artistUpdate.controller";
 import { createArtistController } from "../controllers/artist/createArtist.controller";
 import { getArtistController } from "../controllers/artist/getArtist.controller";
@@ -8,4 +9,5 @@ const artistRouter = Router();
 artistRouter.post("", createArtistController);
 artistRouter.get("/profile",verifyAuthTokenMiddleware, getArtistController)
 artistRouter.patch("", verifyAuthTokenMiddleware, artistUpdateController);
+artistRouter.delete("", verifyAuthTokenMiddleware, deleteArtistController);
 export { artistRouter };
