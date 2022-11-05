ceadc56bac4201ac22af383ab938fb89dd925a15
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2022-11-05 13:51:12 -0300

    feat: controller de criação de artista

---
+++ src/controllers/artist/createArtist.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { createArtistService } from "../../services/artist/createArtist.service";
+
+export const createArtistController = async (req: Request, res: Response) => {
+  const {name,email, password} = req.body
+  const artist = await createArtistService({name,email,password})
+  return res.status(201).json(artist)
+};
