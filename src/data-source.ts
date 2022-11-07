41176bd6ee8ef59304a89dddbb9b59204b5a2289
Artorys <darksoulsgui123@gmail.com>
2022-11-06 22:17:15 -0300

    feat : add test config in data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,22 +1,30 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-  synchronize: false,
-  logging: true,
-  entities:
-    process.env.NODE_ENV === "production"
-      ? ["dist/src/entities/*.js"]
-      : ["src/entities/*.ts"],
-  migrations:
-    process.env.NODE_ENV === "production"
-      ? ["dist/src/migrations/*.js"]
-      : ["src/migrations/*.ts"],
-});
+export const AppDataSource = new DataSource(process.env.NODE_ENV === "test" ?
+    {
+      type : "sqlite",
+      database : ":memory:",
+      entities : ["src/entities/*.ts"],
+      synchronize : true
+    } 
+    :
+    {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      ssl:
+        process.env.NODE_ENV === "production"
+          ? { rejectUnauthorized: false }
+          : false,
+      synchronize: false,
+      logging: true,
+      entities:
+        process.env.NODE_ENV === "production"
+          ? ["dist/src/entities/*.js"]
+          : ["src/entities/*.ts"],
+      migrations:
+        process.env.NODE_ENV === "production"
+          ? ["dist/src/migrations/*.js"]
+          : ["src/migrations/*.ts"],
+    });
 
