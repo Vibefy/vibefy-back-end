fb27258db5bdd3b5a17a5026ea0552a8945f4efc
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-11-07 16:23:24 -0300

    fix: Ajeitando as config do docker

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import "express-async-errors";
 import express, { Request, Response } from "express";
+import "express-async-errors";
 import sessionRoute from "./routes/session.routes";
 import userRoute from "./routes/user.routes";
 import aswRouter from "./routes/aws.routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./swagger.json";
-import admRoutes from "./routes/adm.routes"
+import admRoutes from "./routes/adm.routes";
+import { mailRoute } from "./routes/mail.routes";
 import handleErrorMiddleware from "./middleware/error.middleware";
-import { mailRoute } from "./routes/mail.routes"
 import { artistRouter } from "./routes/artist.routes";
 
 const app = express();
-app.use(express.json())
-app.use("/adm", admRoutes)
+app.use(express.json());
+app.use("/adm", admRoutes);
 app.use("/user", userRoute);
-app.use("/artist", artistRouter)
+app.use("/artist", artistRouter);
 app.use("/login", sessionRoute);
 app.use("/file", aswRouter);
 app.use("/send-email", mailRoute);
