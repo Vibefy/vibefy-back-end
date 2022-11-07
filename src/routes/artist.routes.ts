33998852393e3167329f2aafdfe48eceb4c1458a
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-11-07 17:33:21 -0300

    refactor: Identação e organização

--- src/routes/artist.routes.ts
+++ src/routes/artist.routes.ts
@@ -1,17 +1,40 @@
 import { Router } from "express";
+
+import { artistCreate, artistUpdate } from "../schema/artist";
+import { IArtistRequest, IArtistUpdate } from "../interfaces/artist";
+
+import { getArtistController } from "../controllers/artist/getArtist.controller";
 import { deleteArtistController } from "../controllers/artist/artistDelete.controller";
 import { artistUpdateController } from "../controllers/artist/artistUpdate.controller";
 import { createArtistController } from "../controllers/artist/createArtist.controller";
-import { getArtistController } from "../controllers/artist/getArtist.controller";
-import { IArtistRequest, IArtistUpdate } from "../interfaces/artist";
+
+import { verifyAuthTokenMiddleware } from "../middleware/verifyAuthTokenMiddleware";
 import { schemaValidationMiddleware } from "../middleware/schemaValidation.middleware";
 import { verifyAuthArtistMiddleware } from "../middleware/verifyAuthArtistMiddleware";
-import { verifyAuthTokenMiddleware } from "../middleware/verifyAuthTokenMiddleware";
-import { artistCreate, artistUpdate } from "../schema/artist";
 
 const artistRouter = Router();
-artistRouter.post("",schemaValidationMiddleware<IArtistRequest>(artistCreate),createArtistController);
-artistRouter.get("/profile",verifyAuthTokenMiddleware,verifyAuthArtistMiddleware,getArtistController)
-artistRouter.patch("/profile", verifyAuthTokenMiddleware,verifyAuthArtistMiddleware,schemaValidationMiddleware<IArtistUpdate>(artistUpdate),artistUpdateController);
-artistRouter.delete("/profile", verifyAuthTokenMiddleware,verifyAuthArtistMiddleware,deleteArtistController);
+artistRouter.post(
+  "",
+  schemaValidationMiddleware<IArtistRequest>(artistCreate),
+  createArtistController
+);
+artistRouter.get(
+  "/profile",
+  verifyAuthTokenMiddleware,
+  verifyAuthArtistMiddleware,
+  getArtistController
+);
+artistRouter.patch(
+  "/profile",
+  verifyAuthTokenMiddleware,
+  verifyAuthArtistMiddleware,
+  schemaValidationMiddleware<IArtistUpdate>(artistUpdate),
+  artistUpdateController
+);
+artistRouter.delete(
+  "/profile",
+  verifyAuthTokenMiddleware,
+  verifyAuthArtistMiddleware,
+  deleteArtistController
+);
 export { artistRouter };
