913daa2e98653ff0b16dd4ff0b53595226721009
Victor <ovictoravila@gmail.com>
2022-11-07 18:39:44 -0300

    feat: create deleteMusicPlaylist controller and service

--- src/controllers/playlist/deleteMusicPlaylist.controller.ts
+++ src/controllers/playlist/deleteMusicPlaylist.controller.ts
@@ -1,6 +1,13 @@
 import { Response, Request } from "express";
+import { deleteMusicPlaylistService } from "../../services/playlist/deleteMusicPlaylist.service";
 
 export const deleteMusicPlaylistController = async (
   req: Request,
   res: Response
-) => {};
+) => {
+  const { id, id_music } = req.params;
+
+  await deleteMusicPlaylistService(id, id_music);
+
+  res.status(204).send();
+};
---
+++ src/services/playlist/deleteMusicPlaylist.service.ts
@@ -0,0 +1,31 @@
+import { AppDataSource } from "../../data-source";
+import Music from "../../entities/music.entity";
+import Playlist from "../../entities/playlist.entity";
+import { AppError } from "../../error/appError";
+
+export const deleteMusicPlaylistService = async (
+  id: string,
+  id_music: string
+): Promise<boolean> => {
+  const deleteRepository = AppDataSource.getRepository(Playlist);
+  const musicRepository = AppDataSource.getRepository(Music);
+
+  const playlist = await deleteRepository.findOneBy({ id });
+
+  if (!playlist) {
+    throw new AppError(404, "Playlist not found");
+  }
+
+  const music = await musicRepository.findOneBy({ id: id_music });
+
+  if (!music) {
+    throw new AppError(404, "Music not found");
+  }
+
+  const filteredMusics = playlist.music.filter((elem) => elem.id !== id_music);
+
+  playlist.music = filteredMusics;
+  await deleteRepository.save(playlist);
+
+  return true;
+};
