d10f709eae9d2696991698fdfefeeb79f7295371
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-11-07 17:33:48 -0300

    refactor: Identação e organização

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { sessionController } from "../controllers/user/session.controller";
-import { ILogin } from "../interfaces/session";
-import { schemaValidationMiddleware } from "../middleware/schemaValidation.middleware";
+
 import { login } from "../schema/session";
+import { ILogin } from "../interfaces/session";
+
+import { sessionController } from "../controllers/user/session.controller";
+
+import { schemaValidationMiddleware } from "../middleware/schemaValidation.middleware";
 
 const router = Router();
 router.post("",schemaValidationMiddleware<ILogin>(login),sessionController)
