4352f1108d4b37a066c66607c5a2bcc197ef28a7
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-11-07 17:33:08 -0300

    refactor: Identação e organização

--- src/routes/adm.routes.ts
+++ src/routes/adm.routes.ts
@@ -1,17 +1,34 @@
 import { Router } from "express";
+
+import { admCreate, admUpdate } from "../schema/adm";
+import { IAdmRequest, IAdmUpdate } from "../interfaces/adm";
+
+import { getAdmController } from "../controllers/adm/getAdm.controller";
 import { admUpdateController } from "../controllers/adm/admUpdate.controller";
 import { createAdmController } from "../controllers/adm/createAdm.controller";
-import { getAdmController } from "../controllers/adm/getAdm.controller";
-import { IAdmRequest, IAdmUpdate } from "../interfaces/adm";
-import { schemaValidationMiddleware } from "../middleware/schemaValidation.middleware";
+
 import { verifyAuthAdminMiddleware } from "../middleware/verifyAuthAdminMiddleware";
 import { verifyAuthTokenMiddleware } from "../middleware/verifyAuthTokenMiddleware";
-import { admCreate,admUpdate } from "../schema/adm";
+import { schemaValidationMiddleware } from "../middleware/schemaValidation.middleware";
 
 const router = Router();
-router.post("",schemaValidationMiddleware<IAdmRequest>(admCreate),createAdmController);
-router.get("/profile",verifyAuthTokenMiddleware,verifyAuthAdminMiddleware,getAdmController);
-router.patch("/profile", verifyAuthTokenMiddleware,verifyAuthAdminMiddleware,schemaValidationMiddleware<IAdmUpdate>(admUpdate),admUpdateController);
-
+router.post(
+  "",
+  schemaValidationMiddleware<IAdmRequest>(admCreate),
+  createAdmController
+);
+router.get(
+  "/profile",
+  verifyAuthTokenMiddleware,
+  verifyAuthAdminMiddleware,
+  getAdmController
+);
+router.patch(
+  "/profile",
+  verifyAuthTokenMiddleware,
+  verifyAuthAdminMiddleware,
+  schemaValidationMiddleware<IAdmUpdate>(admUpdate),
+  admUpdateController
+);
 
 export default router;
