cb865de742f85babe30cbc65417d4b7d2373cad1
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-11-08 22:14:46 -0300

    fix: Arrumando erros e indentando

--- src/services/user/playlist/AddPlaylist.service.ts
+++ src/services/user/playlist/AddPlaylist.service.ts
@@ -3,22 +3,23 @@ import { AppError } from "../../../error/appError";
 import { AppDataSource } from "../../../data-source";
 import Playlist from "../../../entities/playlist.entity";
 
-export const addPlaylistUserService = async (id:string, id_playlist:string) => {
-  const userRepository = AppDataSource.getRepository(User)
-  const user = await userRepository.findOneBy({id})
+export const addPlaylistUserService = async (
+  id: string,
+  id_playlist: string
+) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const user = await userRepository.findOneBy({ id });
 
-  const playlistRepository = AppDataSource.getRepository(Playlist)
-  const playlistFind = await  playlistRepository.findOneBy({id: id_playlist})
+  const playlistRepository = AppDataSource.getRepository(Playlist);
+  const playlistFind = await playlistRepository.findOneBy({ id: id_playlist });
 
-  if(!playlistFind){
-    throw new AppError(404, "Playlist not exist")
+  if (!playlistFind) {
+    throw new AppError(404, "Playlist not exist");
   }
-  if(user.playlist.find((elem) => elem.id == playlistFind.id)){
-    throw new AppError(404, "Playlist exist")
+  if (user.playlist.find((elem) => elem.id == playlistFind.id)) {
+    throw new AppError(404, "Playlist exist");
   }
 
-  user.playlist = [...user.playlist, playlistFind]
+  user.playlist = [...user.playlist, playlistFind];
   await userRepository.save(user);
-
-  return 
 };
